d9541dc43d2bd65759be79c62e8960c0f2d0a8d2
GabGabHub <gabibellol342@gmail.com>
2025-12-26 01:41:52 +0200

    Update mongo.py

--- backend/mongo.py
+++ backend/mongo.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 
 
-MONGODB_URL="mongodb+srv://belticvictor22:stud@iotbd.f16qbsn.mongodb.net/"
+MONGODB_URL="secret"
 
 client = MongoClient(MONGODB_URL)
 
 db = client.project
 task_collection = db.get_collection("tasks")
+
 user_collection = db.get_collection("users")
